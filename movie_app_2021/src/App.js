1a5e7cda120b37091196cb827b841a4b5dca9ff1
happystrike1029 <dabinlee1029@gmail.com>
2021-06-02 14:30:55 +0900

    2.1 Reusable Components with JSX + Props

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -1,5 +1,17 @@
 import React from "react";
-import Potato from './Potato';
+
+function Food({fav}){
+  //만약 {fav}를 빼줬더라면 props를 가져와서 props의 fav를 입력해줘야 한다. 
+  //console.log(props);
+  return <h1>I like {fav}</h1>;
+}
+
+/*
+function Food(props){
+  return <h1>I like {props.fav}</h1>;
+}
+*/
+
 
 function App() {
   return (
@@ -8,7 +20,12 @@ function App() {
       <h1>
        Hello~!!
       </h1>
-      <Potato />
+      <Food fav="rice"
+        // something = {true}
+        // papapa = {["hello", 1, 2, 3, 4, true]}
+      />
+      <Food fav="noodle" />
+      <Food fav="soup" />
     </div>
   );
 }
--- movie_app_2021/src/index.js
+++ movie_app_2021/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+/*  StrictMode 는 애플리케이션 내의 잠재적인 문제를 알아내기 위한 도구 */
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode> 
     <App />
   </React.StrictMode>,
   document.getElementById('root')
