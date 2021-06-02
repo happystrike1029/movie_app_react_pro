5e3a10dc8b6bcbdaf5098d32dfb6a0fbca4111f9
happystrike1029 <dabinlee1029@gmail.com>
2021-06-02 17:47:49 +0900

    2.4 Protection with PropTypes

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -1,29 +1,5 @@
 import React from "react";
-/*
-function Food(props){
-  return <h1>I like {props.fav}</h1>;
-}
-*/
-
-// 2.0의 형태 
-// function App() {
-//   return (
-//     // <div className="App"/>
-//     <div> 
-//       <h1>
-//        Hello~!!
-//       </h1>
-//       <Food fav="rice"
-//         // something = {true}
-//         // papapa = {["hello", 1, 2, 3, 4, true]}
-//       />
-//       <Food fav="noodle" />
-//       <Food fav="soup" />
-//     </div>
-//   );
-// }
-// export default App;
-
+import PropTypes from "prop-types";
 // 2.1 렌더링 하는 법 
 const foodILike = [
   {
@@ -31,57 +7,59 @@ const foodILike = [
     name: "Kimchi",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+    ,rating : 5
   },
   {
     id:2,
     name: "Samgyeopsal",
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+    ,rating : 4.5
   },
   {
     id:3,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+    ,rating : 4.7
   },
   {
     id:4,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+    ,rating : 4.4
   },
   {
     id:5,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+    ,rating : 4.6
   }
 ];
 
-// 이렇게도 사용 가능 
-// function renderFood(dish){
-//   return <Food name={dish.name} picture={dish.image} />
-// } 
-
-
-function Food({name, picture}){
-  //만약 {fav}를 빼줬더라면 props를 가져와서 props의 fav를 입력해줘야 한다. 
-  //console.log(props);
+function Food({name, picture, rating}){
   return (
     <div>
     <h1>I like {name}</h1>
+    <h4>{rating}/5.0</h4>
     <img src={picture} alt={name} />
   </div>
   );
 }
 
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating:PropTypes.number.isRequired
+};
 
 function App() {
   return (
-    // <div className="App"/>
     <div> 
       {foodILike.map(dish =>(
-        <Food key={dish.id} name={dish.name} picture={dish.image}/>
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
       ))}
     </div>
     // dish는 object 
