2e5836d94fb156e64ea6d0058d3e9ac1f5ec0d2e
happystrike1029 <dabinlee1029@gmail.com>
2021-06-09 11:02:16 +0900

    4.1 Rendering the Movies

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-class App extends React.Component{
+class App extends React.Component{ // state를 이용하기 위해서 class component를 사용 
   state = {
     isLoading : true, 
     movies : []
   };
 
   getMovies = async() =>{
-    const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    //const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    const {
+      data : {
+        data : {movies}
+      }      
+    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+    //console.log("TEST : ", movies);
+    this.setState({movies, isLoading : false});
   }
 
   componentDidMount(){
@@ -19,10 +28,29 @@ class App extends React.Component{
   }
 
   render(){
-    const {isLoading} = this.state;
-    return <div>
-      {isLoading ? "Loading..." : "We are ready"}
-    </div>;
+    // const {isLoading} = this.state;
+    // return <div>
+    //   {isLoading ? "Loading..." : "We are ready"}
+    // </div>;
+    const {isLoading, movies} = this.state;
+    return(
+    <div>
+      {isLoading ? "Loading..." : movies.map(movie => (
+        // console.log("TEST > ", movie),
+        <Movie
+          key = {movie.id}
+          id = {movie.id}
+          year = {movie.year}
+          title = {movie.title}
+          summary = {movie.summary}
+          poster = {movie.medium_cover_image}
+        />
+      ))}
+    </div>
+    );
+    
+    
+
   }
 }
 
