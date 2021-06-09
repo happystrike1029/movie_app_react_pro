8eb457313e68e21a30a9502f7315e737eaeca652
happystrike1029 <dabinlee1029@gmail.com>
2021-06-09 11:35:57 +0900

    4.2 Styling the Movies

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -32,24 +32,47 @@ class App extends React.Component{ // state를 이용하기 위해서 class comp
     // return <div>
     //   {isLoading ? "Loading..." : "We are ready"}
     // </div>;
+    // const {isLoading, movies} = this.state;
+    // return(
+    // <div>
+    //   {isLoading ? "Loading..." : movies.map(movie => (
+    //     // console.log("TEST > ", movie),
+    //     <Movie
+    //       key = {movie.id}
+    //       id = {movie.id}
+    //       year = {movie.year}
+    //       title = {movie.title}
+    //       summary = {movie.summary}
+    //       poster = {movie.medium_cover_image}
+    //     />
+    //   ))}
+    // </div>
+    // );
+    
+    //4.2 Styling the Movies
     const {isLoading, movies} = this.state;
     return(
-    <div>
-      {isLoading ? "Loading..." : movies.map(movie => (
-        // console.log("TEST > ", movie),
-        <Movie
-          key = {movie.id}
-          id = {movie.id}
-          year = {movie.year}
-          title = {movie.title}
-          summary = {movie.summary}
-          poster = {movie.medium_cover_image}
-        />
-      ))}
-    </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ):(
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie 
+                key = {movie.id}
+                id = {movie.id}
+                year = {movie.year}
+                title = {movie.title}
+                summary = {movie.summary}
+                poster = {movie.medium_cover_image}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
-    
-    
 
   }
 }
