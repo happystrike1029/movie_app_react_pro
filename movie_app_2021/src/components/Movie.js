07dc05398898a376c1d3154f5af94b45d1e8ab8f
happystrike1029 <dabinlee1029@gmail.com>
2021-06-29 10:16:33 +0900

    6.4 Redirecting

--- movie_app_2021/src/components/Movie.js
+++ movie_app_2021/src/components/Movie.js
@@ -5,8 +5,9 @@ import "./Movie.css";
 
 function Movie({id, year, title, summary, poster, genres}){
     return (
+        <div className="movie">
         <Link to={{
-            pathname : "/movie-detail",
+            pathname : "/movie/${id}",
             state : {
                 year, 
                 title,
@@ -15,7 +16,6 @@ function Movie({id, year, title, summary, poster, genres}){
                 genres
             }
         }}>
-        <div className="movie">
             <img src={poster} alt={title} title={title}/>
             <div className="movie_data">
                 <h3 className="movie_title">{title}</h3>
@@ -29,8 +29,8 @@ function Movie({id, year, title, summary, poster, genres}){
                 </ul>
                 <p className="movie_summary">{summary.slice(0, 180)}...</p>
             </div>
-        </div>
         </Link>
+        </div>
     );
 }
 
